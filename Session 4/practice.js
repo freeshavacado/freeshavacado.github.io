e270f29db3df5da67f0f71d775fd95e86754060e
freeshavacado <53665977+freeshavacado@users.noreply.github.com>
2019-08-03 17:34:23 -0500

    Add files via upload
    I AM AWESOME

---
+++ Session 4/practice.js
@@ -0,0 +1,17 @@
+var a =873491827461238741;
+
+if (a>5) {
+  alert("a is greater than 5.");
+}
+if (a==50) {
+  alert("a is 50.");
+}
+else {
+  alert("a is less than or equal to 5.");
+}
+
+var x=0;
+while (true) {
+   alert(x);
+   x=x+1;
+}
